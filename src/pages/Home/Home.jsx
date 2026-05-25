f17519bdac9f87f9e4d22bc9da0983a056ea55f9
Guilherme Guedes Teixeira <163352991+GuiGuedes10@users.noreply.github.com>
2026-05-25 15:24:30 -0300

    upp

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 
 import Button from '@/components/Button'
+import caveirao from '../../assets/caveiraomtfoda.jpg'
 
 function Home() {
   const handleClick = () => {
@@ -7,10 +8,29 @@ function Home() {
   }
 
   return (
-    <main className="min-h-screen flex items-center justify-center">
-      <Button text="ORÇAMENTO" onClick={handleClick} />
-    </main>
+    <>    
+    <section className="flex flex-row max-w-screen-2xl mx-auto justify-center items-center">
+      <div className="flex flex-row justify-center items-center max-w-screen-2xl mx-auto">
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-4xl font-bold">ViboraInk</h1>
+          <p className="text-lg">Cada pele conta uma história. Qual é a sua?</p>
+        </div>
+        <div className="rounded-lg overflow-hidden">
+          <img src={caveirao} alt="caveirao" />
+         </div>
+         <div className="rounded-lg overflow-hidden">
+          <img src={caveirao} alt="caveirao" />
+         </div>
+         <div className="rounded-lg overflow-hidden">
+          <img src={caveirao} alt="caveirao" />
+         </div>
+      </div>
+    </section>
+    </>
   )
 }
 
 export default Home
+
+
+
